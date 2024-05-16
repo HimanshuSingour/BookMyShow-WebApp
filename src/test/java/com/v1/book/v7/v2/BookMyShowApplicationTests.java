5282e13d37cac603128ecee4d64e72d712660164
HimanshuSingour <singourhimanshu3@gmail.com>
2024-05-17 00:09:37 +0530

    first commit

---
+++ src/test/java/com/v1/book/v7/v2/BookMyShowApplicationTests.java
@@ -0,0 +1,13 @@
+package com.v1.book.v7.v2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookMyShowApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
